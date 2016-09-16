5a2e9b8b781c3759778f181e595dc97d5582810c
saisuniltata <saisuniltata@gmail.com>
2016-09-16 13:33:37 -0500

    Route testing1

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ var path    = require("path");
 
 
 //app.use(express.static(__dirname + '/View'));
-app.use(express.static(__dirname + '/assests'));
-app.use(express.static(__dirname ));
+app.use('/', express.static(__dirname + '/public'));
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
