04a20806d6ba5681c1c1ffa6da8322bb14bfdd06
saisuniltata <saisuniltata@gmail.com>
2016-09-18 18:22:15 -0500

    html5 render

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-var express = require("express");
+var express = require('express');
+var bodyParser = require('body-parser')
 var app     = express();
-var path    = require("path");
+var path    = require('path');
 
 
 //app.use(express.static(__dirname + '/View'));
 app.use('/', express.static(__dirname));
-app.get('/',function(req,res){
+app.get('/*',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 var port = process.env.PORT||3000;
 app.listen(port);
+console.log("Node Server Listening on port 3000");
