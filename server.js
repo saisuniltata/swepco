a044eb14865cfbfb3e65a2bc5f46250e38c5023f
saisuniltata <saisuniltata@gmail.com>
2016-09-18 19:08:20 -0500

    route testing3

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.get('/*',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
-app.get('/lubricants/*',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'),{root:__dirname+'/lubricants'});
-    
+app.get('/lubricants/123',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
+
 var port = process.env.PORT||3000;
 app.listen(port);
 console.log("Node Server Listening on port 3000");
