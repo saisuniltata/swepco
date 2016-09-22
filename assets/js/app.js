cdf61f559224a5d463999dcee79d45d7aa75ce8f
saisuniltata <saisuniltata@gmail.com>
2016-09-22 18:55:50 -0500

    code cleanse

--- assets/js/app.js
+++ assets/js/app.js
@@ -684,6 +684,7 @@ myApp.controller('mainController', ['$scope', function ($scope) {
 myApp.controller('contactController', ['$scope', '$http', function ($scope, $http) {
     $.getScript("https://www.google.com/recaptcha/api.js?onload=vcRecaptchaApiLoaded&render=explicit“ async defer");
     var vm = this;
+    vm.publicKey = '';
     var user = {
         "company": ""
         , "firstname": ""
