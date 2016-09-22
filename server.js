f214b43cb54c022a6247a71970112b8e20dd384b
saisuniltata <saisuniltata@gmail.com>
2016-09-22 01:19:32 -0500

    recaptcha test2

--- server.js
+++ server.js
@@ -17,21 +17,26 @@ app.get('/*', function (req, res) {
     //__dirname : It will resolve to your project folder.
 });
 app.post('/contactUs', function (req, res, next) {
+    console.log('started');
     if (req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+        console.log('inside if');
         return res.json({
             "responseCode": 1
             , "responseDesc": "Please select captcha"
         });
     }
+    console.log('passed if');
     // Put your secret key here.
     var secretKey = "6Lf5DwcUAAAAAE8LHG1acRK7X8h7HZ49CdWd5_XU";
     // req.connection.remoteAddress will provide IP address of connected user.
     var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
     // Hitting GET request to the URL, Google will respond with success or error scenario.
     request(verificationUrl, function (error, response, body) {
+        console.log('inside request');
         body = JSON.parse(body);
         // Success will be true or false depending upon captcha validation.
         if (body.success !== undefined && !body.success) {
+            console.log('body unsuccessful');
             return res.json({
                 "responseCode": 1
                 , "responseDesc": "Failed captcha verification"
