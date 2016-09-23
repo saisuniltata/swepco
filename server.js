a07b685cad403e95ad46d1200ebcbb4e7b935b4f
saisuniltata <saisuniltata@gmail.com>
2016-09-23 01:09:42 -0500

    captcha angular test5

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var Recaptcha = require('recaptcha').Recaptcha;
 var bodyParser = require('body-parser');
 var app = express();
 var path = require('path');
@@ -6,6 +7,8 @@ var port = process.env.PORT || 3000;
 var router = express.Router();
 var nodemailer = require('nodemailer');
 var request = require('request');
+var PUBLIC_KEY = '6Lf5DwcUAAAAAF1dChWB09G-dXjVvOVVjfjmx8lt'
+    , PRIVATE_KEY = '6Lf5DwcUAAAAAE8LHG1acRK7X8h7HZ49CdWd5_XU';
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -13,69 +16,51 @@ app.use(bodyParser.urlencoded({
 //app.use(express.static(__dirname + '/View'));
 app.use('/', express.static(__dirname));
 app.get('/*', function (req, res) {
+    var recaptcha = new Recaptcha(PUBLIC_KEY, PRIVATE_KEY);
     res.sendFile(path.join(__dirname + '/index.html'));
     //__dirname : It will resolve to your project folder.
 });
 app.post('/contactUs', function (req, res, next) {
-    console.log('started');
-    console.log(req.body);
-    if (req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
-        console.log('inside if');
-        return res.json({
-            "responseCode": 1
-            , "responseDesc": "Please select captcha"
-        });
-    }
-    console.log('passed if');
-    // Put your secret key here.
-    var secretKey = "6Lf5DwcUAAAAAE8LHG1acRK7X8h7HZ49CdWd5_XU";
-    // req.connection.remoteAddress will provide IP address of connected user.
-    var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
-    // Hitting GET request to the URL, Google will respond with success or error scenario.
-    request(verificationUrl, function (error, response, body) {
-        console.log('inside request');
-        body = JSON.parse(body);
-        // Success will be true or false depending upon captcha validation.
-        if (body.success !== undefined && !body.success) {
-            console.log('body unsuccessful');
-            return res.json({
-                "responseCode": 1
-                , "responseDesc": "Failed captcha verification"
+            console.log('started');
+            console.log(req.body);
+            var data = {
+                remoteip: req.connection.remoteAddress
+                , challenge: req.body.recaptcha_challenge_field
+                , response: req.body.recaptcha_response_field
+            };
+            var recaptcha = new Recaptcha(PUBLIC_KEY, PRIVATE_KEY, data);
+            recaptcha.verify(function (success, error_code) {
+                if (success) {
+                    console.log('request body ends');
+                    //Node Mailer starts
+                    var transporter = nodemailer.createTransport({
+                        service: 'yahoo'
+                        , auth: {
+                            user: 'sunil_fire_ice@yahoo.com'
+                            , pass: 'S#2303unil'
+                        }
+                        , tls: {
+                            rejectUnauthorized: false
+                        }
+                    });
+                    var mailOptions = {
+                        from: 'Swepco Lubes<sunil_fire_ice@yahoo.com>'
+                        , to: req.body.email
+                        , subject: 'Swepco lubes'
+                        , text: 'Swepco testing text'
+                        , html: '<p>Swepco testing text</p>'
+                    };
+                    transporter.sendMail(mailOptions, function (error, info) {
+                        if (error) {
+                            console.log(error);
+                            res.redirect('/');
+                        }
+                        else {
+                            console.log('Message sent:' + info.response);
+                            res.redirect('/contactUs');
+                        }
+                    })
+                }
             });
-        }
-        res.json({
-            "responseCode": 0
-            , "responseDesc": "Sucess"
-        });
-    });
-    console.log('request body ends');
-    var transporter = nodemailer.createTransport({
-        service: 'yahoo'
-        , auth: {
-            user: 'sunil_fire_ice@yahoo.com'
-            , pass: 'S#2303unil'
-        }
-        , tls: {
-            rejectUnauthorized: false
-        }
-    });
-    var mailOptions = {
-        from: 'Swepco Lubes<sunil_fire_ice@yahoo.com>'
-        , to: req.body.email
-        , subject: 'Swepco lubes'
-        , text: 'Swepco testing text'
-        , html: '<p>Swepco testing text</p>'
-    };
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-            res.redirect('/');
-        }
-        else {
-            console.log('Message sent:' + info.response);
-            res.redirect('/contactUs');
-        }
-    })
-});
-app.listen(port);
-console.log("Node Server Listening on port 3000");
+            app.listen(port);
+            console.log("Node Server Listening on port 3000");
