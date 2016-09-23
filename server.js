083fb7de32a38cb17af4bca61eba4ea6932e22a4
saisuniltata <saisuniltata@gmail.com>
2016-09-23 01:13:01 -0500

    captcha angular test7

--- server.js
+++ server.js
@@ -21,46 +21,47 @@ app.get('/*', function (req, res) {
     //__dirname : It will resolve to your project folder.
 });
 app.post('/contactUs', function (req, res, next) {
-            console.log('started');
-            console.log(req.body);
-            var data = {
-                remoteip: req.connection.remoteAddress
-                , challenge: req.body.recaptcha_challenge_field
-                , response: req.body.recaptcha_response_field
-            };
-            var recaptcha = new Recaptcha(PUBLIC_KEY, PRIVATE_KEY, data);
-            recaptcha.verify(function (success, error_code) {
-                if (success) {
-                    console.log('request body ends');
-                    //Node Mailer starts
-                    var transporter = nodemailer.createTransport({
-                        service: 'yahoo'
-                        , auth: {
-                            user: 'sunil_fire_ice@yahoo.com'
-                            , pass: 'S#2303unil'
-                        }
-                        , tls: {
-                            rejectUnauthorized: false
-                        }
-                    });
-                    var mailOptions = {
-                        from: 'Swepco Lubes<sunil_fire_ice@yahoo.com>'
-                        , to: req.body.email
-                        , subject: 'Swepco lubes'
-                        , text: 'Swepco testing text'
-                        , html: '<p>Swepco testing text</p>'
-                    };
-                    transporter.sendMail(mailOptions, function (error, info) {
-                        if (error) {
-                            console.log(error);
-                            res.redirect('/');
-                        }
-                        else {
-                            console.log('Message sent:' + info.response);
-                            res.redirect('/contactUs');
-                        }
-                    })
+    console.log('started');
+    console.log(req.body);
+    var data = {
+        remoteip: req.connection.remoteAddress
+        , challenge: req.body.recaptcha_challenge_field
+        , response: req.body.recaptcha_response_field
+    };
+    var recaptcha = new Recaptcha(PUBLIC_KEY, PRIVATE_KEY, data);
+    recaptcha.verify(function (success, error_code) {
+        if (success) {
+            console.log('request body ends');
+            //Node Mailer starts
+            var transporter = nodemailer.createTransport({
+                service: 'yahoo'
+                , auth: {
+                    user: 'sunil_fire_ice@yahoo.com'
+                    , pass: 'S#2303unil'
+                }
+                , tls: {
+                    rejectUnauthorized: false
                 }
             });
-            app.listen(port);
-            console.log("Node Server Listening on port 3000");
+            var mailOptions = {
+                from: 'Swepco Lubes<sunil_fire_ice@yahoo.com>'
+                , to: req.body.email
+                , subject: 'Swepco lubes'
+                , text: 'Swepco testing text'
+                , html: '<p>Swepco testing text</p>'
+            };
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                    res.redirect('/');
+                }
+                else {
+                    console.log('Message sent:' + info.response);
+                    res.redirect('/contactUs');
+                }
+            })
+        }
+    });
+});
+app.listen(port);
+console.log("Node Server Listening on port 3000");
